0ccc30015e2343a547966dd1a794c00a27d5c4ab
Tyler Gaw <me@tylergaw.com>
2014-10-26 21:25:28 -0400

    Getting a couple static routes in place

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'static_pages/about'
+  get 'static_pages/shows'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
