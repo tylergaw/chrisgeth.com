995310d4293f706c1b3d93ad1b57fe502fe1e717
Tyler Gaw <me@tylergaw.com>
2014-11-22 19:41:31 -0500

    Updating all of the page-title heads and CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,10 @@ module ApplicationHelper
     return time_ago_in_words(time).gsub(/about|less than|almost|over/, '')
       .strip << append
   end
+
+  def title_char_class()
+    chars = ['andy', 'freddie', 'hills', 'morrissey', 'multiman', 'rickman',
+      'roddy', 'willem']
+    return "title-#{chars.sample}"
+  end
 end
