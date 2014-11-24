7526b6ef63b406fc26adf0c9d5e1d3f1af6940a2
Tyler Gaw <me@tylergaw.com>
2014-11-24 09:59:10 -0500

    silly, no-good, hacky workaround for paperclip-s3

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,14 @@
 class Video < ActiveRecord::Base
-  has_attached_file :image
+  # NOTE: For some reason the s3 config stuff HAS to be set here as well as in
+  # the config. No idea why, it's stupid
+  has_attached_file :image,
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+
   validates_attachment_content_type :image, :content_type =>
     ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
