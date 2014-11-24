3ad7a783b111b4110269fe9b3495b737e8f3b4cf
Tyler Gaw <me@tylergaw.com>
2014-11-24 08:15:26 -0500

    app helper for rev assets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,15 @@ module ApplicationHelper
       'roddy', 'willem']
     return "title-#{chars.sample}"
   end
+
+  def rev_manifest(asset_type, asset_key)
+    file_name = "#{Rails.public_path + asset_type}/rev-manifest.json"
+    json_text = File.read(file_name)
+    json = JSON.parse(json_text)
+    return json[asset_key]
+  end
+
+  def rev_manifest_css(filename)
+    return "/css/#{rev_manifest("css", filename)}"
+  end
 end
