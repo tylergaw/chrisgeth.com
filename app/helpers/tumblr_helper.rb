f5f4fd4e49c3f3b2efb840b42086369e4aa2fe03
Tyler Gaw <me@tylergaw.com>
2014-12-07 02:18:58 -0500

    Correct dates on articles

--- app/helpers/tumblr_helper.rb
+++ app/helpers/tumblr_helper.rb
@@ -2,4 +2,9 @@ module TumblrHelper
   def tb_client
     tb_client = Tumblr::Client.new :consumer_key => ENV['TUMBLR_KEY']
   end
+
+  def get_post_by_id(id)
+    res = tb_client.posts('thechrisgethardshow.tumblr.com', :id => id)
+    res["posts"][0]
+  end
 end
