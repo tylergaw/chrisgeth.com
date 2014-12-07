0805effc0d873f8a75018ac270a052d813660445
Tyler Gaw <me@tylergaw.com>
2014-12-07 00:18:18 -0500

    remove the html thing from the gulp watcher

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,6 +2,6 @@ var gulp = require("gulp"),
   config = require("../config");
 
 gulp.task("watch", function () {
-  gulp.watch([config.assets.fonts.src, config.assets.images.src, config.assets.html.src], ["copy-static"]);
+  gulp.watch([config.assets.fonts.src, config.assets.images.src], ["copy-static"]);
   gulp.watch(config.scss.src, ["scss"]);
 });
