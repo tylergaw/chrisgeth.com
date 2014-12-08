ca26069464452e7e46a0a83df5a60e0e7ce47a79
Tyler Gaw <me@tylergaw.com>
2014-12-08 09:48:34 -0500

    Good-enough for now on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   include TumblrHelper
 
   def index
-    @posts = res = tb_client.posts(tb_url, limit: 1)['posts']
-    @show = Show.where("show_date >= ?", Time.zone.now.to_date).first
+    @posts = res = tb_client.posts(tb_url, limit: 2)['posts']
+    @shows = Show.where("show_date >= ?", Time.zone.now.to_date).limit(3)
   end
 end
