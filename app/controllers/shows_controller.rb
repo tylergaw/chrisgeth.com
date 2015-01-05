b6dd017f7a5f78b43f4fd18bb4547c5077d39649
Tyler Gaw <me@tylergaw.com>
2015-01-05 09:21:59 -0500

    Sort by show_date not created_date

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,24 +1,26 @@
 class ShowsController < ApplicationController
   layout :resolve_layout
 
+  # TODO: Clean this up a bit so you don't have to order() on every one
+
   def index
-    @shows = Show.where("show_date >= ?", Time.zone.now.to_date)
+    @shows = Show.where("show_date >= ?", Time.zone.now.to_date).order("show_date")
   end
 
   def shows_by_year
-    @shows = Show.where('extract(year from show_date) = ?', params[:year])
+    @shows = Show.where('extract(year from show_date) = ?', params[:year]).order("show_date")
     render :template => "shows/index"
   end
 
   def shows_by_month
     @shows = Show.where('extract(year from show_date) = ? and extract(month from show_date) = ?',
-      params[:year], params[:month])
+      params[:year], params[:month]).order("show_date")
     render :template => "shows/index"
   end
 
   def shows_by_day
     @shows = Show.where('extract(year from show_date) = ? and extract(month from show_date) = ? and extract(day from show_date) = ?',
-      params[:year], params[:month], params[:day])
+      params[:year], params[:month], params[:day]).order("show_date")
     render :template => "shows/index"
   end
 
